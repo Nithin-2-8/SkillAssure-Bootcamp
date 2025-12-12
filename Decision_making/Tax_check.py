a85ebefd67b14e0cfe738e4200757e9d50d93285
Nithin-2-8 <nithin28904@gmail.com>
2025-12-12 23:19:16 +0530

    7 done

---
+++ Decision_making/Tax_check.py
@@ -0,0 +1,25 @@
+"""
+Challenge 7: Salary & Tax Eligibility Check
+Rule: Salary > 3,00,000 must pay tax.
+"""
+def main():
+    name = input("Enter Employee Name: ")
+    salary_str = input("Enter Annual Salary in Rs: ")
+    
+    try:
+        salary = float(salary_str)
+        
+        print(f"\nChecking tax status for {name}...")
+    except ValueError:
+        print("Invalid name input")    
+    try:
+        if salary > 300000:
+            print("Result: You must pay tax.")
+        else:
+            print("Result: You are NOT required to pay tax.")
+            
+    except ValueError:
+        print("Invalid salary input.")
+
+if __name__ == "__main__":
+    main()
